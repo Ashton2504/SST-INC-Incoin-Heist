b89d0295f5cf6e88788507da35c536254020aae3
Ashton Lim <ashton_lim_jing_kai@s2024.ssts.edu.sg>
2024-10-20 20:49:40 +0800

    Changed

---
+++ OneViewView.swift
@@ -0,0 +1,18 @@
+//
+//  OneViewView.swift
+//  Incoin Heist
+//
+//  Created by Ashton Lim on 20/10/24.
+//
+
+import SwiftUI
+
+struct OneViewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OneViewView()
+}
